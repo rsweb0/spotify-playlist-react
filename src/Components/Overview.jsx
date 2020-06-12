084487da015c441658bde9a545538f0f221f5d05
rsweb0 <rswebstar267@gmail.com>
2020-06-12 18:59:57 +0530

    Added Loader to the Overview

--- src/Components/Overview.jsx
+++ src/Components/Overview.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import {Button} from "@uprise/button";
 import style from "../css/overview.module.css";
+import Loader from 'react-loader-spinner'
 
 var profile = "";
 
 export const Overview = (props) => {
   profile = props.profile;
   let image = props.images ? props.images[0].url : "";
-  let displayName = props.displayName ? props.displayName : "Loading...";
+  let displayName = props.displayName ? props.displayName : 
+  <Loader
+         type="ThreeDots"
+         color="#00BFFF"
+         height={100}
+         width={100}
+         timeout={3000}  
+      />
+   ;
+  
   let followers = props.followers ? `Followers(${props.followers})` : "";
 
   return (
